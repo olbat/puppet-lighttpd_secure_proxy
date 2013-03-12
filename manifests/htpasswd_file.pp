76535e337a3faa1336249e26b4a254ef72f08fba
Luc Sarzyniec <luc.sarzyniec@gmail.com>
2013-03-12 23:43:57 +0100

    Updated documentation

--- manifests/htpasswd_file.pp
+++ manifests/htpasswd_file.pp
@@ -1,3 +1,23 @@
+# = lighttpd_secure_proxy::htpasswd_file
+# 
+# Copy htpasswd files in the lighttpd install directory.
+# 
+# == Parameters: 
+#
+# $srcfile:: the file that have to be copied
+# $destfile:: the filename of the file in the destination directory (the lighttpd configuration path)
+#
+# == Requires: 
+# 
+# Nothing.
+# 
+# == Sample Usage:
+#
+#  lighttpd_secure_proxy::htpasswd_file{'bt.passwd':
+#    srcfile => 'puppet:///modules/lighttpd_secure_proxy/bt.passwd',
+#    destfile => 'bt.passwd',
+#  }
+#
 define lighttpd_secure_proxy::htpasswd_file($srcfile,$destfile) {
   include lighttpd_secure_proxy
 
